1635fba0f26cb13eb2d06d5174a0ecd066a7b54f
Matheus Antonelli de Oliveira <matheusantonelli12@gmail.com>
2023-04-13 23:00:44 -0300

    Update Main-Section of home view.

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import Header from '../components/Header/Header'
+import { Button } from 'dracula-ui'
 
 function Home() {
   return (
-    <div>
-      <Header/>
-      Hello World@!
-      Applicativo de Curriculos 
+    <div className='main-section'>
+      <h1>Criar curriculos nunca foi tão facil!</h1>
+      <Button color='purple'>Comece seu curriculo</Button>
     </div>
   )
 }
 
 export default Home
+ 
